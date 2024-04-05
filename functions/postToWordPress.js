3afa2969a1be2c4d87d9aaf0ed5572ff327464c4
lellopis <leo.llopis@etu.uca.fr>
2024-04-05 19:37:07 +0200

    fix: detect when empty article and fixed slug when parenthese in product name

--- functions/postToWordPress.js
+++ functions/postToWordPress.js
@@ -18,7 +18,11 @@ async function postToWordPress(htmlContent, productName, categoryId, postStatus,
         body: JSON.stringify({
           title: "Fiche Technique - " + productName,
           // Slug has to be "fiche-technique-" + the product name normalized without spaces and accents, and dashes instead of spaces"
-          slug: "fiche-technique-" + formattedProductName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g, "-"),
+          slug: "fiche-technique-" + formattedProductName.normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "") // Supprimer les accents
+            .replace(/\(/g, "") // Supprimer les parenthèses ouvrantes
+            .replace(/\)/g, "") // Supprimer les parenthèses fermantes
+            .replace(/ /g, "-"), // Remplacer les espaces par des tirets
           content: htmlContent,
           status: postStatus,
           categories: [categoryId, 2653],
--- functions/updateToWordPress.js
+++ functions/updateToWordPress.js
@@ -22,7 +22,11 @@ async function updateToWordPress(htmlContent, productName, postId, postStatus, c
         },
         body: JSON.stringify({
           title: "Fiche Technique - " + productName,
-          slug: "fiche-technique-" + formattedProductName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/ /g, "-"),
+          slug: "fiche-technique-" + formattedProductName.normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "") // Supprimer les accents
+            .replace(/\(/g, "") // Supprimer les parenthèses ouvrantes
+            .replace(/\)/g, "") // Supprimer les parenthèses fermantes
+            .replace(/ /g, "-"), // Remplacer les espaces par des tirets
           content: htmlContent,
           categories: [categoryId, 2653],
           excerpt: metaDesc
